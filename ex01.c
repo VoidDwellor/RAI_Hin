cabbcd8b3b04171aaa4882045fc5cf1c4e0fa4c2
VoidDwellor <lookhinzaza3004@gmail.com>
2025-07-08 03:16:52 +0000

    ex01.c

---
+++ ex01.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    printf("My name is : %s\n", "KMITL");
+    printf("My point is : %d\n", 20+34+30);
+    printf("Grade : %c\n", 'A');
+    printf("GPA : %f", 3.99);
+}
