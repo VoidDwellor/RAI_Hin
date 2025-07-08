e8f5c0172efa631111e126cf8fe74c4ac55df18f
VoidDwellor <lookhinzaza3004@gmail.com>
2025-07-08 04:07:17 +0000

    15minquiz.c

--- 15minquiz.c
+++ 15minquiz.c
@@ -6,12 +6,32 @@
 #include<stdio.h>
 #include<math.h>
 
+/*
 int main()
 {
-    printf("Programming : %c\n", 'A');
-    printf("Drawing : %s\n", "B+");
-    printf("Mechanics : %s\n", "B+");
-    printf("Math : %c\n", 'B');
-    printf("GPS : %f\n", 3.5);
+    int a, b, c, d, e;
+    printf("Programming : ");
+    scanf("%s", &a);
+    printf("Drawing : ");
+    scanf("%s", &b);
+    printf("Mechanics : ");
+    scanf("%s", &c);
+    printf("Math : ");
+    scanf("%s", &d);
+    printf("GPS : ");
+    scanf("%d", &e);
+    return 0;
+}
+*/
+
+int main()
+{
+    int a, b, c;
+    printf("First value : ");
+    scanf("%d", &a);
+    printf("Second value : ");
+    scanf("%d", &b);
+    c = a + b;
+    printf("%d + %d = %d\n");
     return 0;
 }
