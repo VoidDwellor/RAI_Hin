e73f034eab1df017513014163aecae412af8a5e1
VoidDwellor <lookhinzaza3004@gmail.com>
2025-07-08 03:40:54 +0000

    ex01.c

--- ex01.c
+++ ex01.c
@@ -1,11 +1,19 @@
 #include<stdio.h>
 #include<math.h>
 
+//15 Minute quizzes :
+
+//1. Machine laguage is a lowest from of coding language that consists of only binary.
+
+//2. 2048
+
+//3.
 int main()
 {
-    printf("My name is : %s\n", "KMITL");
-    printf("My point is : %d\n", 20+34+30);
-    printf("Grade : %c\n", 'A');
-    printf("GPA : %f", 3.99);
+    printf("Programming : %c\n", 'A');
+    printf("Drawing : %s\n", "B+");
+    printf("Mechanics : %s\n", "B+");
+    printf("Math : %c\n", 'B');
+    printf("GPS : %f\n", 3.5);
     return 0;
 }
